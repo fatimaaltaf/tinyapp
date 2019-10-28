44c3f1eaa935987bcf765c671a06389ad782116d
Fatima Altaf <fatima.altaf@gmail.com>
2019-10-28 21:48:11 +0000

    added body parser code to make post request human readable

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -16,6 +19,11 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
